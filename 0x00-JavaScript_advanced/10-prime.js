88cb36ccbbd6ffdb19a3f590e58cbbc3f658b28c
mdallen5393 <allen_matthew@outlook.com>
2023-03-14 12:11:47 -0500

    Task 10. Execution stack & timing execution

---
+++ 0x00-JavaScript_advanced/10-prime.js
@@ -0,0 +1,24 @@
+function countPrimeNumbers () {
+  let count = 0;
+  for (let i = 2; i < 100; i++) {
+    if (isPrime(i))
+    count++;
+  }
+  return;
+}
+
+function isPrime (num) {
+  if (num % 1) return false;
+  const sqr = Math.sqrt(num);
+  
+  for (let i = 2; i < sqr; i++)
+  if (sqr % i == 0) return false;
+  
+  return true;
+}
+
+let start = performance.now();
+for (let i = 0; i < 100; i++)
+  countPrimeNumbers();
+time = performance.now() - start;
+console.log(`Execution time of printing countPrimeNumbers was ${time} milliseconds`);
