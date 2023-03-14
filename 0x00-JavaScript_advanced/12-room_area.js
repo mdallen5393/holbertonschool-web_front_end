a02479a7a156f4d5d66f548c13210c05c07c7d38
mdallen5393 <allen_matthew@outlook.com>
2023-03-14 13:14:44 -0500

    Task 12. Binding

---
+++ 0x00-JavaScript_advanced/12-room_area.js
@@ -0,0 +1,10 @@
+const roomDimensions = {
+  width: 50,
+  length: 100,
+  getArea: function() {
+    return this.width * this.length;
+  }
+};
+
+const boundGetArea = roomDimensions.getArea.bind(roomDimensions);
+console.log(boundGetArea());
